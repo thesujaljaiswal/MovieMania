63194d6c49dc45fb7abb3dfee243dc45b2bbe8d5
Yash Jaiswal <yashjaiswal2121@gmail.com>
2023-10-14 11:51:38 +0530

    Update ratingSlice.js

--- src/store/ratingSlice.js
+++ src/store/ratingSlice.js
@@ -4,15 +4,20 @@ export const raingSlice = createSlice({
   name: "ratings",
   initialState: {
     rating: {},
+    changing:false
   },
   reducers: {
     setRatings:(state,action)=>{
+        state.changing=true;
         state.rating=action.payload;
+    },
+    removeAlert:(state)=>{
+      state.changing=false;
     }
     
   },
 });
 
-export const { setRatings } = raingSlice.actions;
+export const { setRatings,removeAlert } = raingSlice.actions;
 
 export default raingSlice.reducer;
